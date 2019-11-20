2037ca6ba961a4c50a3b3df83b3ef21c0ca8ba37
Callum-James Smith <techdragon@tuta.io>
2019-11-20 17:21:28 +0000

    imported example project and setup packages

---
+++ src/display/Game.java
@@ -0,0 +1,58 @@
+package display;
+
+import logic.KeyboardListener;
+import logic.MouseClickListener;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Game extends JFrame {
+    private JPanel grid_panel; // panel used to display grid panels
+    private JPanel[][] grid_array; // array used to store panels in grid
+
+    public void initialise() {
+        // if grid already drawn (reinitialise rather than initialise) remove grid from frame and delete it
+        if (grid_panel != null) {
+            this.remove(grid_panel);
+            grid_panel = null;
+        }
+
+        grid_panel = new JPanel(new GridLayout(20, 20));
+        grid_array = new JPanel[20][20];
+
+        // for loop to create grid
+        for (int x = 0; x < grid_array.length; x++) {
+            for (int y = 0; y < grid_array[x].length; y++) {
+                grid_array[x][y] = new JPanel();
+
+                // checkered design black and white
+                if ((x % 2 == 0 && y % 2 == 1) || (x % 2 == 1 && y % 2 == 0)) {
+                    grid_array[x][y].setBackground(Color.BLACK);
+                } else {
+                    grid_array[x][y].setBackground(Color.WHITE);
+                }
+
+                // unique mouse listener per panel to determine which panel was clicked
+                grid_array[x][y].addMouseListener(new MouseClickListener(this));
+                grid_panel.add(grid_array[x][y]);
+            }
+        }
+
+        // add panel to frame
+        this.add(grid_panel);
+    }
+
+    public Game() {
+
+        // set up game environment
+        initialise();
+        // key listener to respond to key events
+        addKeyListener(new KeyboardListener(this));
+
+        // standard configuration
+        setSize(new Dimension(400, 400));
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+
+    }
+}
--- src/display/Main.java
+++ src/display/Main.java
@@ -1,8 +1,8 @@
-package GUI;
+package display;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    new Game();
     }
 }
