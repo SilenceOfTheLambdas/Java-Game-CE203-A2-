71591ab12a4ceab0a52fd15d6cf01ef6cb354466
Callum-James Smith <techdragon@tuta.io>
2019-12-11 21:06:48 +0000

    countdown now shows on bottom panel

---
+++ src/logic/PlayerScores.java
@@ -0,0 +1,6 @@
+package logic;
+
+public class PlayerScores {
+//    TODO: Create a players score file
+//    - Write to it & then, either add a new player and their score, or update an existing players score if it's higher than there last
+}
